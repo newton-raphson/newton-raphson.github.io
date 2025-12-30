851883eb0f53037ea76b6645d757f506ec559bb8
Samundra Karki <samundrakarki@Samundras-Laptop.local>
2025-12-29 20:29:03 -0500

    more academic add

--- src/routes/projects.js
+++ src/routes/projects.js
@@ -38,29 +38,29 @@ const Projects = () => {
         "https://github.com/NirajanBekoju/Wine-Quality-Classification/blob/master/Report/main.pdf",
       github_link: "https://github.com/NirajanBekoju/Wine-Quality-Classification",
     },
-    {
-      title: "Fourier Series Drawing",
-      keywords: ["Fourier Series", "DFT", "Epicycles"],
-      description:
-        "Fourier series is an expansion of a periodic function in terms of an infinite sum of sines and cosines. Fourier series make use of the orthogonality relationships of the sine and cosine functions. With appropriate weights, one cycle or period of the summation can be made to approximate an arbitrary function in that interval. In mathematics, the discrete-time fourier transform (DTFT) is a form of Fourier analysis that is applicable to the sequence of values. The DTFT is often used to analyze samples of a continuous function. The term discrete-time refers to the fact that the transform operates on discrete data, often samples whose interval has units of time. In this program, we are going to use discrete fourier transform in order to generate the fourier series of the given discrete coordinate data. In mathematics, the discrete Fourier Transform (DFT) converts a finite sequence of equally-spaced samples of function into a same-length sequence of equal-spaced samples of the discrete-time fourier transform(DTFT), which is a complex valued function of frequency.",
-      github_link: "https://github.com/NirajanBekoju/Fourier-Transform-Drawing",
-    },
-    {
-      title: "Epidemic Modelling",
-      keywords: ["SIR Model", "C++", "OpenGL", "Data Analysis", "Data Visualization"],
-      description:
-        "(Compartmental Model in Epidemiology ) Epidemic modelling is the mathematical model to illustrate the spread of epidemic disease. We have use SIR model in order to study the epidemic disease transmission. SIR model is a compartmental model in epidemiology. In this model, the population is assigned to compartments with labels - for example :- S, I or R(SUSCEPTIBLE, INFECTIOUS or RECOVERED). People may progress between compartments.",
-      github_link: "https://github.com/NirajanBekoju/Epidemic-Modelling",
-    },
-    {
-      title: "Malignant and Benign Tumor Diagnosis",
-      keywords: ["Tumor", "pandas", "seaborn", "sk-learn", "Keras"],
-      description:
-        "Using the dataset obtained from kaggle for malignant and benign tumor diagonosis, in this project a model is built to predict the type of tumor and obtained 97.902% validation accuracy using keras sequential model.",
-      document_link: "",
-      github_link:
-        "https://github.com/NirajanBekoju/Malignant-and-Benign-Tumor-Diagnosis",
-    },
+    // {
+    //   title: "Fourier Series Drawing",
+    //   keywords: ["Fourier Series", "DFT", "Epicycles"],
+    //   description:
+    //     "Fourier series is an expansion of a periodic function in terms of an infinite sum of sines and cosines. Fourier series make use of the orthogonality relationships of the sine and cosine functions. With appropriate weights, one cycle or period of the summation can be made to approximate an arbitrary function in that interval. In mathematics, the discrete-time fourier transform (DTFT) is a form of Fourier analysis that is applicable to the sequence of values. The DTFT is often used to analyze samples of a continuous function. The term discrete-time refers to the fact that the transform operates on discrete data, often samples whose interval has units of time. In this program, we are going to use discrete fourier transform in order to generate the fourier series of the given discrete coordinate data. In mathematics, the discrete Fourier Transform (DFT) converts a finite sequence of equally-spaced samples of function into a same-length sequence of equal-spaced samples of the discrete-time fourier transform(DTFT), which is a complex valued function of frequency.",
+    //   github_link: "https://github.com/NirajanBekoju/Fourier-Transform-Drawing",
+    // },
+    // {
+    //   title: "Epidemic Modelling",
+    //   keywords: ["SIR Model", "C++", "OpenGL", "Data Analysis", "Data Visualization"],
+    //   description:
+    //     "(Compartmental Model in Epidemiology ) Epidemic modelling is the mathematical model to illustrate the spread of epidemic disease. We have use SIR model in order to study the epidemic disease transmission. SIR model is a compartmental model in epidemiology. In this model, the population is assigned to compartments with labels - for example :- S, I or R(SUSCEPTIBLE, INFECTIOUS or RECOVERED). People may progress between compartments.",
+    //   github_link: "https://github.com/NirajanBekoju/Epidemic-Modelling",
+    // },
+    // {
+    //   title: "Malignant and Benign Tumor Diagnosis",
+    //   keywords: ["Tumor", "pandas", "seaborn", "sk-learn", "Keras"],
+    //   description:
+    //     "Using the dataset obtained from kaggle for malignant and benign tumor diagonosis, in this project a model is built to predict the type of tumor and obtained 97.902% validation accuracy using keras sequential model.",
+    //   document_link: "",
+    //   github_link:
+    //     "https://github.com/NirajanBekoju/Malignant-and-Benign-Tumor-Diagnosis",
+    // },
   ];
 
   return (
